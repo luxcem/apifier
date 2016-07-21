156f8a22169c8634faeef7f8cb687adf2e11f721
luxcem <a@luxcem.fr>
2016-07-21 15:37:44 +0200

    Bump 1.0

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name = 'apifier',
     packages = ['apifier'],
-    version = '0.1',
+    version = '1.0.0',
     description = 'A web parser for tabular and/or paginated data',
     author = 'luxcem',
     author_email = 'a@luxcem.fr',
     url = 'https://github.com/luxcem/apifier',
-    download_url = 'https://github.com/luxcem/apifier/tarball/0.1',
+    download_url = 'https://github.com/luxcem/apifier/tarball/1.0.0',
     keywords = ['api', 'parser', 'table data', 'html parser'],
     classifiers = [],
 )
