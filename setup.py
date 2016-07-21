6ee3e80a49805b9f0ccd258b5a97e4f3c0bf1d14
luxcem <a@luxcem.fr>
2016-07-21 16:30:41 +0200

    v1.0.5

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name = 'apifier',
     packages = ['apifier'],
-    version = '1.0.4',
+    version = '1.0.5',
     description = 'A web parser for tabular and/or paginated data',
     author = 'luxcem',
     author_email = 'a@luxcem.fr',
     url = 'https://github.com/luxcem/apifier',
-    download_url = 'https://github.com/luxcem/apifier/tarball/1.0.4',
+    download_url = 'https://github.com/luxcem/apifier/tarball/1.0.5',
     keywords = ['api', 'parser', 'table data', 'html parser'],
     classifiers = [],
     install_requires=[
